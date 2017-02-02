3b5f14d41f8005cdeb9a142d71f393e4273d4864
mjmcginley <2141683m@student.gla.ac.uk>
2017-02-02 16:21:35 +0000

    Completed chapter 6

--- rango/views.py
+++ rango/views.py
@@ -47,9 +47,10 @@ def index(request):
     # that will be passed to the template engine.
 
     category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
 
     # Construct a dictionary to pass to the template engine as it's context.
-    context_dict = {'categories':category_list}
+    context_dict = {'categories':category_list, 'pages':pages_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
