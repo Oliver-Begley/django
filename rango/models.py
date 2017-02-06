4bf58d61267679eccb8eb9cba37571b1c6398315
mjmcginley <2141683m@student.gla.ac.uk>
2017-02-06 20:31:15 +0000

    Completed up to and including 9.6 Creating the register view

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.template.defaultfilters import slugify
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -36,3 +36,17 @@ class Page(models.Model):
         return self.title
     def __unicode__(self):
         return self.title
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images',blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username
